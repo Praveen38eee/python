0ec41f3f6aeda5cc81b3c7818c7467a990a53a79
Praveen38eee <138850593+Praveen38eee@users.noreply.github.com>
2023-07-09 17:25:32 +0530

    Update 5th experiment.py

--- 5th experiment.py
+++ 5th experiment.py
@@ -1,3 +1,5 @@
+''' Python program to check if a set is a subset of another set.WITHOUT USING issubset function()'''
+
 sample = (1, 2, 3, 4, 5)
 reverse = tuple(reversed(sample))
 print("Original Tuple:", sample)
