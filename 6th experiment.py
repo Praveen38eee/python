9824756cba821a39dba90ed6ed744f3583c045ea
Praveen38eee <138850593+Praveen38eee@users.noreply.github.com>
2023-07-09 17:28:01 +0530

    Update 6th experiment.py

--- 6th experiment.py
+++ 6th experiment.py
@@ -1,4 +1,4 @@
-'''Write a Python program to check if a set is a subset of another set.WITHOUT USING issubset function()'''
+'''Python program to check if a set is a subset of another set.WITHOUT USING issubset function()'''
 
 Grocery = {"Cornflakes":{"quantity":15,"price":100},"Muesli":{"quantity":14,"price":150},"Oats":{"quantity":10,"price":80}
 ,"Wheat Flakes": {"quantity": 5,"price" : 75}
